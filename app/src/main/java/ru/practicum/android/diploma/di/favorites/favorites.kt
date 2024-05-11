f27f7717635ee4c2ad7b9105325adc4d83aae2c1
Mikhail.Pavlov <7links91@mail.ru>
2024-05-11 17:12:19 +0300

    GH-6

---
+++ app/src/main/java/ru/practicum/android/diploma/di/favorites/favorites.kt
@@ -0,0 +1,16 @@
+package ru.practicum.android.diploma.di.favorites
+
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import ru.practicum.android.diploma.data.db.AppDatabase
+
+val favorites = module {
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database")
+            .allowMainThreadQueries()
+            .build()
+    }
+
+}
