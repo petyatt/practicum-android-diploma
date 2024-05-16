4722e3c98861f96af81250710bdbb0a33c0a8b0d
Mike Pavlov <7links91@mail.ru>
2024-05-17 01:10:37 +0300

    Правки для бота

--- app/src/main/java/ru/practicum/android/diploma/data/dto/ContactsDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/ContactsDto.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma.data.dto
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+
 @Parcelize
 data class ContactsDto(
     val name: String? = null,
--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -5,16 +5,17 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import ru.practicum.android.diploma.data.dto.Response
 import ru.practicum.android.diploma.data.network.request.RequestVacancies
+import java.io.IOException
 
 class RetrofitNetworkClient(
-    private val headHunterApiService: HeadHunterApiService,
-    private val context: Context
+    private val headHunterApiService: HeadHunterApiService, private val context: Context
 ) : NetworkClient {
     override suspend fun doRequest(dto: Any): Response {
         if (!isConnected()) {
-            return Response().apply { resultCode = -1 }
+            return Response().apply { resultCode = NO_INTERNET_ERROR }
         }
 
         return withContext(Dispatchers.IO) {
@@ -31,19 +32,27 @@ class RetrofitNetworkClient(
                             onlyWithSalary = dto.onlyWithSalary
                         )
 
-                        Response().apply { resultCode = 200 }
+                        Response().apply { resultCode = SUCCESS }
                     }
 
                     else -> {
-                        Response().apply { resultCode = 400 }
+                        Response().apply { resultCode = CLIENT_ERROR }
                     }
                 }
 
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Response().apply { resultCode = SERVER_ERROR }
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                Response().apply { resultCode = SERVER_ERROR }
+            } catch (e: RuntimeException) {
+                e.printStackTrace()
+                Response().apply { resultCode = SERVER_ERROR }
             } catch (e: Exception) {
                 e.printStackTrace()
-                Response().apply { resultCode = 500 }
+                Response().apply { resultCode = SERVER_ERROR }
             }
-
 
         }
     }
@@ -52,15 +61,20 @@ class RetrofitNetworkClient(
         val connectivityManager = context.getSystemService(
             Context.CONNECTIVITY_SERVICE
         ) as ConnectivityManager
-        val capabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         if (capabilities != null) {
             when {
                 capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
                 capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
             }
         }
         return false
+    }
+
+    companion object {
+        private const val CLIENT_ERROR = 400
+        private const val SERVER_ERROR = 500
+        private const val NO_INTERNET_ERROR = -1
+        private const val SUCCESS = 200
     }
 }
