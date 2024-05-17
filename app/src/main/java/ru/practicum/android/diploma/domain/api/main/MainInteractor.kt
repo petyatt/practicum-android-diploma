23c9797895ce31a8ec13f30b9fa13b364e9e343f
petyatt <petya_07@bk.ru>
2024-05-17 22:20:28 +0700

    GH-48: deleted Unused import

--- app/src/main/java/ru/practicum/android/diploma/domain/api/main/MainInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/main/MainInteractor.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.domain.api.main
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.Vacancies
 import ru.practicum.android.diploma.domain.models.VacancyDescription
-import ru.practicum.android.diploma.util.Resource
 
 interface MainInteractor {
 
