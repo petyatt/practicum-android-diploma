18069edfb7c7025239ce8cef327f257979cbb3a0
AlexeyMurenkov <murenkovalexey@gmail.com>
2024-05-20 18:14:21 +0300

    GH-41: Исправления merge-онфликта

--- app/src/main/java/ru/practicum/android/diploma/di/VacancyModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/VacancyModule.kt
@@ -1,14 +1,21 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.data.impl.vacancy.HeadHunterVacanciesRepositoryImpl
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.domain.api.HeadHunterVacanciesRepository
 import ru.practicum.android.diploma.domain.api.vacancy.VacancyInteractor
 import ru.practicum.android.diploma.domain.impl.VacancyInteractorImpl
 import ru.practicum.android.diploma.ui.vacancy.VacancyViewModel
 
 val vacancy = module {
+    single<NetworkClient> {
+        RetrofitNetworkClient(headHunterApiService = get(), androidContext())
+    }
+
     single<HeadHunterVacanciesRepository> {
         HeadHunterVacanciesRepositoryImpl()
     }
