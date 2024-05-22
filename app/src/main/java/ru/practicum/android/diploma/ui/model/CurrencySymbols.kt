82f568a4d42563e18f3afa05ea015b1bcb15c35e
AlexeyMurenkov <murenkovalexey@gmail.com>
2024-05-23 01:05:50 +0300

    refactor: Форматирование кода

--- app/src/main/java/ru/practicum/android/diploma/ui/model/CurrencySymbols.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/model/CurrencySymbols.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.ui.model
 val CURRENCY_SYMBOLS = mapOf(
     "RUR" to "\u20BD",
     "RUB" to "\u20BD",
-    "BYR" to  "Br",
+    "BYR" to "Br",
     "USD" to "$",
     "KZT" to "₸",
     "UAH" to "₴",
